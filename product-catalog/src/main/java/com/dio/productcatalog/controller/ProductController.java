72e39922aa842e0efd7fb19821ac2e551c0b06a8
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2021-09-05 19:10:30 -0300

    Started product-catalog and shopping cart

---
+++ product-catalog/src/main/java/com/dio/productcatalog/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.dio.productcatalog.controller;
+
+import com.dio.productcatalog.model.Product;
+import com.dio.productcatalog.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+//Don't forget the @RestController!
+@RestController
+@RequestMapping(value="/product")
+public class ProductController {
+
+    //Initialize repository:
+    @Autowired
+    private ProductRepository productRepository;
+
+    @RequestMapping(method= RequestMethod.POST)
+    Product create(@RequestBody Product product){
+        return productRepository.save(product);
+    }
+
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    Optional<Product> findById(@PathVariable Integer id){
+        return productRepository.findById(id);
+    }
+}
